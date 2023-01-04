7f60b75895dc97889b6a0b3fc3601936fb66950b
rocco1967 <gianfranco.fa@gmail.com>
2023-01-04 10:39:38 +0100

    Update OPENAI.py

--- OPENAI.py
+++ OPENAI.py
@@ -7,8 +7,9 @@ This is a temporary script file.
 import openai
 import pandas as pd
 import streamlit as st
-
-openai.api_key = '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'
+import os
+openai.api_key = os.environ["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"]
+#openai.api_key = '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'
 model_engine = "text-davinci-003"
 prompt = 'consigli per la corsa'
 completions = openai.Completion.create(
@@ -19,5 +20,5 @@ completions = openai.Completion.create(
     stop=None,
     temperature=0.9,
 )
-st.write( completions.choices[0].text)
+st.write(completions.choices[0].text)
 #message = (completions.choices[0].text)
