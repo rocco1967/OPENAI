294b5de64f4994e1f54e2721b35814788895ebfc
rocco1967 <gianfranco.fa@gmail.com>
2023-01-04 10:46:35 +0100

    Update OPENAI.py

--- OPENAI.py
+++ OPENAI.py
@@ -8,7 +8,7 @@ import openai
 import pandas as pd
 import streamlit as st
 import os
-os.environ["OPENAI_API_KEY"] = <"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">
+os.environ["OPENAI_API_KEY"] = "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"
 #openai.api_key = os.environ["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"]
 #openai.api_key = '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'
 model_engine = "text-davinci-003"
