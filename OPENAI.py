b3bee912da8971d4f29c964e37b597d8d9c9e667
rocco1967 <gianfranco.fa@gmail.com>
2023-01-04 16:50:35 +0100

    Update OPENAI.py

--- OPENAI.py
+++ OPENAI.py
@@ -11,9 +11,9 @@ import os
 #openai.api_key = os.getenv("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")
 #os.environ["OPENAI_API_KEY"] = "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"
 #openai.api_key = os.environ["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"]
-openai.api_key = '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'
+openai.api_key = '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'
 model_engine = "text-davinci-003"
-prompt ='consigli per la corsa:'# st.text_input('inserisci la richiesta:')
+prompt =st.text_input('inserisci la richiesta:')
 completions = openai.Completion.create(
     engine=model_engine,
     prompt=prompt,
