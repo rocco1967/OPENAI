aeb52a25fd9328d6a12ae8f79edd21efbf7c4478
rocco1967 <gianfranco.fa@gmail.com>
2023-01-04 17:08:47 +0100

    Update OPENAI.py

--- OPENAI.py
+++ OPENAI.py
@@ -11,7 +11,7 @@ import os
 #openai.api_key = os.getenv("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")
 #os.environ["OPENAI_API_KEY"] = "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"
 #openai.api_key = os.environ["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"]
-openai.api_key_path = r'OPENAI_KEY2.txt'
+openai.api_key_path = 'https://github.com/rocco1967/OPENAI/blob/main/OPENAI_KEY2.txt'
 #openai.api_key = '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'
 model_engine = "text-davinci-003"
 prompt =st.text_input('inserisci la richiesta:')
