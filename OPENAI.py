c637b3d30002a03a5ebd54610fdc2e6ad2f698a0
rocco1967 <gianfranco.fa@gmail.com>
2023-01-12 11:33:39 +0100

    Update OPENAI.py

--- OPENAI.py
+++ OPENAI.py
@@ -29,17 +29,7 @@ audio_bytes = audio_recorder()####
 if audio_bytes:######
     st.audio(audio_bytes, format="audio/wav")######
 audio=  st.audio(audio_bytes, format="audio/wav")   
-r = sr.Recognizer()    
-with sr.AudioFile('audio.waw') as source:
-    audio_text = r.listen(source)
-# recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
-    try:
-        # using google speech recognition
-        text = r.recognize_google(audio_text)
-        st.write('Converting audio transcripts into text ...')
-        st.write(text)
-    except:
-         st.write('Sorry.. run again...')#######    
+
     
     
     
