07c6297e3d3b496a9775b6af254a9d069f415a8e
rocco1967 <gianfranco.fa@gmail.com>
2023-01-13 10:09:31 +0100

    Update OPENAI.py

--- OPENAI.py
+++ OPENAI.py
@@ -59,7 +59,7 @@ a=files.name
 #st.write(files.name)#, bytes_data)
 with open(os.path.join("/tmp", files.name), "wb") as f:
     f.write(bytes_data)  # write this content elsewhere
-with open(os.path("/tmp"),"r") as r:
+with open(os.path("/tmp",a),"rb") as r:
     st.download_button(data=r)
 #model = whisper.load_model('base')
 #st.text('language recognition model loaded')
